d0ab21548697fe7f89a99c47ccddc9ee422a06f6
Fabio Marsiaj <fabiobackend@gmail.com>
2024-05-04 13:35:32 -0300

    find and create order

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma/prisma.service';
+import { OrderDto } from './order.dto';
+import { OrderStatus } from '.prisma/client/orders';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prismaService: PrismaService) {}
+
+  all() {
+    return this.prismaService.order.findMany();
+  }
+
+  create(data: OrderDto) {
+    return this.prismaService.order.create({
+      data: {
+        ...data,
+        status: OrderStatus.PENDING,
+      },
+    });
   }
 }
